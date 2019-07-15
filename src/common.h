d1ab8d5d0e4d300b49e609d0e3135facb8d0fa79
Alex Beregszaszi <alex@rtfs.hu>
2019-07-16 00:31:54 +0100

    Some naive (inlining) optimisations for interp

--- src/common.h
+++ src/common.h
@@ -115,7 +115,7 @@ static const Index kInvalidIndex = ~0;
 static const Offset kInvalidOffset = ~0;
 
 template <typename Dst, typename Src>
-Dst Bitcast(Src&& value) {
+inline Dst Bitcast(Src&& value) {
   static_assert(sizeof(Src) == sizeof(Dst), "Bitcast sizes must match.");
   Dst result;
   memcpy(&result, &value, sizeof(result));
